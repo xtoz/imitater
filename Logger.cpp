3c5a3af2e9df009f27314a6ff38b072e6d290edf
xtoz <401135153@qq.com>
2023-03-11 17:34:24 +0800

    add __FUNCTION__ in Log.

--- Logger.cpp
+++ Logger.cpp
@@ -1,8 +1,50 @@
 #include "Logger.h"
 #include <iostream>
+#include <string.h>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 
 using namespace imitater;
 using namespace std;
+
+LoggerAgent::LoggerAgent(const char* fileName, char codeLine, const char* function, LogLevel level)
+{
+    ::memset(_prefix, PREFIX_LEN, '\0');
+
+    // system time str
+    chrono::system_clock::time_point now = chrono::system_clock::now();
+    time_t now_time = chrono::system_clock::to_time_t(now);
+    tm* now_tm = std::localtime(&now_time);
+    strftime(_prefix, PREFIX_LEN, "%Y-%m-%d %H:%M:%S", now_tm);
+
+    // construct prefix: time + file + codeline
+    sprintf_s(_prefix + strlen(_prefix), sizeof(_prefix), ", level: %s, file: %s, line: %c, function: %s. ",
+     levelToStr(level), fileName, codeLine, function);
+}
+
+const char *LoggerAgent::levelToStr(LoggerAgent::LogLevel level)
+{
+    switch (level)
+    {
+    case FATAL:
+        return "FATAL";
+    case ERRORme:
+        return "ERROR";
+    case WARN:
+        return "WARN";
+    case NORMAL:
+        return "NORMAL";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+LoggerAgent& LoggerAgent::operator<<(const char *str)
+{
+    Logger::getInstace()->log({_prefix, str});
+    return *this;
+}
 
 Logger* Logger::loggerInstance = new Logger(); // consider multi-thread;
 
--- Logger.h
+++ Logger.h
@@ -2,9 +2,8 @@
 #define IMITATER_LOGGER_H
 
 #include "Buffer.h"
-#include <string.h>
 #include <initializer_list>
-#include <windows.h>
+
 
 #define PREFIX_LEN 200
 
@@ -30,46 +29,20 @@ class LoggerAgent
 public:
     enum LogLevel {FATAL,ERRORme,WARN,NORMAL};  // seems cannot use 'ERROR', compiler will call false, do not know why.
 
-    explicit LoggerAgent(const char* fileName, char codeLine, LogLevel level)
-    {
-        SYSTEMTIME time;
-        ::GetLocalTime(&time);
-        sprintf_s(_prefix, "%04d/%02d/%02d %02d:%02d:%02d, level: %s, file: %s, line: %c. ",
-                  time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute, time.wSecond,
-                  levelToStr(level), fileName, codeLine, PREFIX_LEN);
-    }
+    explicit LoggerAgent(const char* fileName, char codeLine, const char* function, LogLevel level);
 
-    const char* levelToStr(LogLevel level)
-    {
-        switch (level)
-        {
-        case FATAL:
-            return "FATAL";
-        case ERRORme:
-            return "ERROR";
-        case WARN:
-            return "WARN";
-        case NORMAL:
-            return "NORMAL";
-        default:
-            return "UNKNOWN";
-        }
-    }
+    const char* levelToStr(LogLevel level);
 
-    LoggerAgent& operator<<(const char* str)
-    {
-        Logger::getInstace()->log({_prefix, str});
-        return *this;
-    }
+    LoggerAgent& operator<<(const char* str);
     
 private:
     char _prefix[PREFIX_LEN];
 };
 }
 
-#define LOG_NORMAL imitater::LoggerAgent(__FILE__, __LINE__, imitater::LoggerAgent::LogLevel::NORMAL)
-#define LOG_WARN imitater::LoggerAgent(__FILE__, __LINE__, imitater::LoggerAgent::LogLevel::WARN)
-#define LOG_ERROR imitater::LoggerAgent(__FILE__, __LINE__, imitater::LoggerAgent::LogLevel::ERRORme)
-#define LOG_FATAL imitater::LoggerAgent(__FILE__, __LINE__, imitater::LoggerAgent::LogLevel::FATAL)
+#define LOG_NORMAL imitater::LoggerAgent(__FILE__, __LINE__, __FUNCTION__, imitater::LoggerAgent::LogLevel::NORMAL)
+#define LOG_WARN imitater::LoggerAgent(__FILE__, __LINE__, __FUNCTION__, imitater::LoggerAgent::LogLevel::WARN)
+#define LOG_ERROR imitater::LoggerAgent(__FILE__, __LINE__, __FUNCTION__, imitater::LoggerAgent::LogLevel::ERRORme)
+#define LOG_FATAL imitater::LoggerAgent(__FILE__, __LINE__, __FUNCTION__, imitater::LoggerAgent::LogLevel::FATAL)
 
 #endif
